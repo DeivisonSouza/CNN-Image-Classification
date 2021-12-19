69d36681910c0eba377474cd8269c4ed3ea00bdb
DeivisonSouza <souzaflorestal@gmail.com>
2021-12-19 11:04:45 -0300

    Initial script

---
+++ R/CNN-Wood.R
@@ -0,0 +1,5 @@
+#--------------------------------------------------------------------------
+#' Author: Deivison V Souza
+#' Institution: Federal University Pará
+#' Analyze: Wood Species Identification using Convolutional Neural Network
+#---------------------------------------------------------------------------
