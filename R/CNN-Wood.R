91bef4c2a00691372e31370447ce1daaee915609
DeivisonSouza <souzaflorestal@gmail.com>
2021-12-19 11:12:37 -0300

    Update script

--- R/CNN-Wood.R
+++ R/CNN-Wood.R
@@ -2,4 +2,7 @@
 #' Author: Deivison V Souza
 #' Institution: Federal University Pará
 #' Analyze: Wood Species Identification using Convolutional Neural Network
+#' DataSet: Macroscopic Images of Wood (Souza et al., 2020)
+#' Manuscript: An automatic recognition system of Brazilian flora species based on textural features of macroscopic images of wood
+#' Journal: Wood Science and Technology
 #---------------------------------------------------------------------------
